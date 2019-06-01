85a8ec4c50dfb9e3bc3aa0d38af3fb1dd3f77a0e
ToraNova <chia_jason96@live.com>
2019-06-01 13:47:02 -0400

    TODO: one passwordfile per user, add it as list to the mqtt.conf

--- pkg/msgapi/mqtt/client.py
+++ pkg/msgapi/mqtt/client.py
@@ -32,7 +32,11 @@ class RapidClientThread( threading.Thread ):
     def __init__(self):
         threading.Thread.__init__(self)
         self.pub_ready = threading.Event()
+        self.reset_client()
+
+    def reset_client(self):
         self.client = mqtt.Client("LOCAL:RapidClient") #client creation
+        self.client.sublist = []
 
     def load_config(self, uname, passwd, addr, portn ):
         '''allows uname, passwd and portn to be loaded and
@@ -45,6 +49,23 @@ class RapidClientThread( threading.Thread ):
         self.pub_ready.clear()
         self.pub_ready.clear()
         self.runflag = False
+
+    def refresh_subs(self):
+        for s in self.client.sublist:
+            self.client.unsubscribe( s )
+        sublist = MQTT_Sub.query.all()
+        client.sublist = []
+        for s in sublist:
+            res = client.subscribe( s.topic )
+            if(res[0] == 0):
+                srvlog["oper"].info("LOCAL:RapidClient subscribed to topic:"+s.topic)
+                sockemit("/mqttctl","mqttqstat_cast",\
+                        {'statstring':"LOCAL:RapidClient subscribed to topic:"+s.topic}, 
+                        eroom='mqttctl')
+                client.sublist.append( s.topic )
+            else:
+                srvlog["oper"].warning("LOCAL:RapidClient subscription failed:"+str(res))
+
 
     def status(self):
         pass
@@ -71,32 +92,33 @@ class RapidClientThread( threading.Thread ):
                 MQTT_Broker_Configuration.config_name == "use_ssl").one()
 
         self.client.connflag = False
-        self.client.username_pw_set(username=self.uname, password= self.passwd) #set auth
+        self.client.username_pw_set(username=self.uname, password=self.passwd) #set auth
         if( ssl_en.config_value in ['True','true',1,'1'] ):
+            # TODO fix ssl error here
             self.client.tls_set( ca_certs = const.SSL_CA, cert_reqs = ssl.CERT_REQUIRED,\
                     tls_version=ssl.PROTOCOL_TLS)
         self.client.on_connect = on_connect
         self.client.on_disconnect = on_disconnect
         self.client.on_message = on_message
-        time.sleep(3)
         while self.runflag: #try until stop running
             try:
-                self.client.connect(self.addr, self.portn, 60) #connect
-            except Exception as e:
-                print("[EX]",__name__," : ","Exception occurred while trying to connect to broker",str(e))
-                srvlog["oper"].error("Exception occurred while trying to connect to broker on {}:{}.".format(self.addr,self.portn)+str(e))
                 time.sleep(3)
-        self.client.loop_start() #start the mqtt loop
+                self.client.connect(self.addr, self.portn, 60) #connect
+                break
+            except Exception as e:
+                print("[EX]",__name__," : ",\
+                        "Exception occurred while trying to connect to broker",str(e))
+                srvlog["oper"].error(\
+                        "Exception occurred while trying to connect to broker on {}:{}.".format(self.addr,self.portn)+str(e))
+                time.sleep(3)
         sockemit("/mqttctl","mqttqstat_cast",\
                 {'statstring':"LOCAL:RapidClient starting"}, \
                 eroom='mqttctl')
 
+        self.client.loop_start()
         while not self.client.connflag:
             #wait until client is connected
             time.sleep(3)
-            sockemit("/mqttctl","mqttqstat_cast",\
-                    {'statstring':"LOCAL:RapidClient connection failed. retrying"}, 
-                    eroom='mqttctl')
             if( not self.runflag ):
                 #exit immediately
                 return
@@ -125,20 +147,23 @@ def on_message(client, userdata, msg):
     print("Msg from topic",msg.topic," : ",msg.payload)
 
 def on_connect(client, userdata, flags, rc):
-    client.connflag = True
-    client.connrc = str(rc)
     srvlog["oper"].info("LOCAL:RapidClient connected with rc:"+str(rc))
     sockemit("/mqttctl","mqttqstat_cast",\
             {'statstring':"LOCAL:RapidClient connected with rc:"+str(rc)}, 
             eroom='mqttctl')
-    sublist = MQTT_Sub.query.all()
-    for s in sublist:
-        res = client.subscribe( s.topic )
-        print("DEBUG:",res)
-        srvlog["oper"].info("LOCAL:RapidClient subscribed to topic:"+s.topic)
-        sockemit("/mqttctl","mqttqstat_cast",\
-                {'statstring':"LOCAL:RapidClient subscribed to topic:"+s.topic}, 
-                eroom='mqttctl')
+    if(rc == 0):
+        client.connflag = True
+        client.connrc = str(rc)
+        sublist = MQTT_Sub.query.all()
+        client.sublist = []
+        for s in sublist:
+            res = client.subscribe( s.topic )
+            # TODO: check if subscription is successfuly
+            srvlog["oper"].info("LOCAL:RapidClient subscribed to topic:"+s.topic)
+            client.sublist.append( s.topic )
+    elif(rc == 5):
+        srvlog["oper"].warning("LOCAL:RapidClient unauthorized connection to broker")
+
 
 def on_disconnect(client, userdata, rc):
     client.connflag = False
